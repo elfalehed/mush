422aa37aab79eba8b8659413c9171bcd7cb20182
KMx404 <mohamedelfaleh5@gmail.com>
2020-08-17 01:13:41 +0100

    :zap: Core edited. Fsociety mood

--- mush.py
+++ mush.py
@@ -1,29 +1,81 @@
 #! /usr/bin/env python3 
-
-# intro Mush: A multiple hash generator. 
-# basically doing encrypting & decrypting comes later 
+# utf-8 
+'''
+ / / Thanks to my @0xrick for the refreshing ideas  
+/ / Greetz to @v1s1t0r the creator of @Airgeddo for contacting Me. 
+'''
 
 # importing Hashs  
 import hashlib 
+import os, sys 
 from hashlib import md5
-from mosecodelib import text
+from zlib import *
+import socket 
+import random 
+import glob 
+import base64 
+import time 
+import getpass 
+from colorama import * 
+from sys import argv
+from time import gmtime, strftime, sleep 
+
+HEADER = '\033[95m'
+IMPORTANT = '\33[35m'
+NOTICE = '\033[33m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+RED = '\033[91m'
+END = '\033[0m'
+UNDERLINE = '\033[4m'
+LOGGING = '\33[34m'
+
+color_random=[HEADER,IMPORTANT,NOTICE,OKBLUE,OKGREEN,WARNING,RED,END,UNDERLINE,LOGGING]
+random.shuffle(color_random)
+mushlogo = color_random[0] + '''
+           __  _____  _______ 
+   / |  / / / / / ___// / / /
+  / /|_/ / / / /\__ \/ /_/ / 
+ / /  / / /_/ /___/ / __  /  
+/_/  /_/\____//____/_/ /_/         
+'''
+
+mushprompt = "mush ~# "
+continuePrompt = "\nClick [Return] to continue"
+
+class mush: 
+   
+        print(mushlogo + RED + ''' 
+        [*] The Quiter You Become, The More You Are Able To Hear! 
+        [!] Mush is a multiple hashing tool. ENJOY hashing things! 
+        ''' + END +'''
+        {1} -- HASHING 
+        {2} -- DECRYPTING
+        {3} -- ENCRYPTING 
+        {4} -- ABOUT  \n
 
 
+
+        ''')
+        choice = input(mushprompt) 
+        clearSrc() 
+        if choice == "1": 
+            Dmd5()
+        elif choice == "2": 
+            DSHA1()
 
 # decrypting using md5 
-def Dmd5():
+def Emd5():
     string = input("Enter the word/sentence you want to decrypt?\n") 
     result = hashlib.md5(string.encode())
 
     print("the Hexadecimal equivalent of hash is: ", end="") 
     print(result.hexdigest())
-
-def Dmorse(): 
-
+ 
 
 
-
-def fun(): 
+def fun():
     dd = open("promtps/intro1.txt", "r") 
     ddd = dd.read() 
     print(ddd) 
@@ -37,26 +89,13 @@ def cod():
     print(zzz) 
     ol = input("$  ") 
     if ol == 1: 
-        Dmorse()
+        Emorse()
     elif ol == 2:
-        Dbinary()
-    
-
-
-
-# where it all makes sense 
-def main():
-    logo = open("banner.txt", "r")
-    intro = open("prompts/intro.txt","r") 
-    bn = logo.read()
-    intrO = intro.read() 
-    print(bn)
-    print(intrO) 
-    pp = input("$  ") 
-    if pp == 1 : 
-        fun()
-    else: 
-        cod() 
-
-
-if __name__=='__main__':main() 
+        Ebinary()
+   
+if __name__=='__main__': 
+    try:
+       mush()
+    except KeyboardInterrupt: 
+        print(" SOMETHIGN WRONG! ...\n") 
+        time.sleep(0.25) 
